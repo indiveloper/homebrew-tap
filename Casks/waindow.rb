7f01562e54b25b3decef30490ac5a38f0d145805
indiveloper <191252253+indiveloper@users.noreply.github.com>
2026-08-02 20:28:24 +0900

    release: update Homebrew cask to Waindow 1.7.7

--- Casks/waindow.rb
+++ Casks/waindow.rb
@@ -1,6 +1,6 @@
 cask "waindow" do
-  version "1.7.6"
-  sha256 "aa247af2613166676528bf250273520cea7fca256f9e96ab1903a51fb59d8536"
+  version "1.7.7"
+  sha256 "ce28024fc221908e6d7387be007b1f44868c32d575e72a37cc79952ce8a5601e"
 
   url "https://github.com/indiveloper/waindow-releases/releases/download/v#{version}/Waindow-#{version}.zip",
       verified: "github.com/indiveloper/waindow-releases/"
